9fd62468e72d3eedcdc924168bdb97e79178b789
Janusz Wedolowski <gleaken@gmail.com>
2024-03-19 20:58:15 +0100

    fix lines and set catpuccin

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -1,9 +1,9 @@
-return 
+return
 {
-   "catppuccin/nvim", 
-   name = "catppuccin", 
+   "catppuccin/nvim",
+   name = "catppuccin",
    priority = 1000,
    config = function()
-     -- vim.cmd.colorscheme "catppuccin"
+      vim.cmd.colorscheme "catppuccin"
    end
 }
