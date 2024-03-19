ebc687f798f2277ae2dee7a80dea7cbf3ba98ae7
Janusz Wedolowski <gleaken@gmail.com>
2024-03-19 22:12:19 +0100

    more lsp config

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
       "williamboman/mason-lspconfig.nvim",
       config = function()
          require("mason-lspconfig").setup({
-            ensure_installed = { "lua_ls", "pylsp" }
+            ensure_installed = { "lua_ls", "pylsp", "tsserver" }
          })
       end
    },
@@ -19,6 +19,10 @@ return {
          local lspconfig = require("lspconfig")
          lspconfig.lua_ls.setup({})
          lspconfig.pylsp.setup({})
+         lspconfig.tsserver.setup({})
+         vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+         vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+         vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
       end
    }
 }
