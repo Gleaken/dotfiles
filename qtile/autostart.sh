399991878ad1b9aba6efce26d213f01b203720dd
Janusz Wedolowski <gleaken@gmail.com>
2024-05-09 00:05:23 +0200

    kb layout and dunst

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -3,5 +3,7 @@
 lxsession &
 picom --daemon &
 xwallpaper --stretch /home/janusz/Pictures/skull_and_bones_2018_video_game-wallpaper-3840x1200.jpg &
+setxkbmap -layout pl &
+dunst &
 #insync start &
 #ckb-next -b
