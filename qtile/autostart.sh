fd22da27a38efb9cfb07245d2b04d37cf17ba86f
Janusz Wedolowski <gleaken@gmail.com>
2024-05-09 19:32:32 +0200

    fix dunst start and picom config file added

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 #
 lxsession &
-picom --daemon -config ~/.config/dotfiles/dunst/dunstrc &
+picom --daemon &
 xwallpaper --stretch /home/janusz/Pictures/skull_and_bones_2018_video_game-wallpaper-3840x1200.jpg &
 setxkbmap -layout pl &
-dunst &
+dunst -config ~/.config/dotfiles/dunst/dunstrc &
 #insync start &
 #ckb-next -b
