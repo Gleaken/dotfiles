897244e2e1d65867958fd2113460d99cb2dbf9a0
Janusz Wedolowski <gleaken@gmail.com>
2024-05-22 19:03:44 +0200

    use stow

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -2,10 +2,10 @@
 #
 xrandr --rate 120
 lxsession &
-picom --daemon --config ~/.config/dotfiles/picom/picom.conf &
+picom --daemon --config ~/.config/picom/picom.conf &
 #xwallpaper --stretch /home/janusz/Pictures/skull_and_bones_2018_video_game-wallpaper-3840x1200.jpg &
 find ~/Pictures/wallpapers/ -type f | shuf -n 1 | xargs xwallpaper --stretch &
 setxkbmap -layout pl &
-dunst -config ~/.config/dotfiles/dunst/dunstrc &
+dunst -config ~/.config/dunst/dunstrc &
 #insync start &
 #ckb-next -b
