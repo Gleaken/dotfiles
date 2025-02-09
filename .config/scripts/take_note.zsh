05ebfcd63181f9cf13e935e9683b39a3ef20880c
Janusz Wedolowski <gleaken@gmail.com>
2025-02-09 12:05:00 +0100

    notes adjustments

--- .config/scripts/take_note.zsh
+++ .config/scripts/take_note.zsh
@@ -5,4 +5,4 @@ datetime=$(date +"%Y-%m-%d_%H-%M")
 # Example: use it as a part of filename
 filename="note_${datetime}.md"
 
-kitty --app-id=kitty-note zsh -c "nvim ~/Documents/obsidian/mynotes/$filename"
+kitty --app-id=kitty-note zsh -c "cd ~/Documents/obsidian/mynotes && nvim"
