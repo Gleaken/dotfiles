710a796eace2f407ad402fd406ef18e15aa5462f
Janusz <gleaken@gmail.com>
2026-01-16 19:36:38 +0100

    some changes

--- .config/scripts/take_note.zsh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/zsh
-
-datetime=$(date +"%Y-%m-%d_%H-%M")
-
-# Example: use it as a part of filename
-filename="note_${datetime}.md"
-
-kitty --app-id=kitty-note zsh -c "cd ~/Documents/obsidian/mynotes && nvim"
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,12 +1,14 @@
 return {
    "nvim-treesitter/nvim-treesitter",
    build = ":TSUpdate",
+   lazy = false,
    config = function ()
       local config = require("nvim-treesitter.configs")
       config.setup({
          ensure_instaled = {"lua", "javascript", "c_sharp", "yaml", "python"},
          highlight = { enable = true },
-         indent = { enable = true }
+       indent = { enable = true },
+         auto_install = true
       })
    end
 }
